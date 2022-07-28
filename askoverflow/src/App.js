dbd36dced4146bd69305ab7bff9b4b9a23a79e2a
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-07-28 23:47:37 +0530

    Added test

--- askoverflow/src/App.js
+++ askoverflow/src/App.js
@@ -6,6 +6,7 @@ import CreateBook from './components/CreateBook';
 import ShowBookList from './components/ShowBookList';
 import ShowBookDetails from './components/ShowBookDetails';
 import UpdateBookInfo from './components/UpdateBookInfo';
+import testbook from './components/testbook';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
           <Route path='/create-book' component={CreateBook} />
           <Route path='/edit-book/:id' component={UpdateBookInfo} />
           <Route path='/show-book/:id' component={ShowBookDetails} />
+          <Route path='/testing' component={testbook} />
         </div>
       </Router>
     );
---
+++ askoverflow/src/components/testbook.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import BookCard from './BookCard';
+
+class testbook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:8082/api/books')
+      .then(res => {
+        this.setState({
+          books: res.data
+        })
+      })
+      .catch(err =>{
+        console.log('Error from ShowBookList');
+      })
+  };
+
+
+
+	render(){
+		const books = this.state.books;
+		let data
+		if(!books) {
+	      data = "there is no book record!";
+	    } else {
+	      data = books.map((book, k) =>
+	        <BookCard book={book} key={k} />
+	      );
+	    }
+		return(
+			<div>
+				<h1>This is our function</h1>
+				<div>
+				{data}
+				</div>
+			</div>
+		);
+	}
+
+ }
+
+ export default testbook;
