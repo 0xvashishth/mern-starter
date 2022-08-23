4df50d948a83970c00733b65be42708613cb033f
Dharmesh Poriya <84856389+Dharmesh-Poriya07@users.noreply.github.com>
2022-08-23 09:59:56 +0000

    last committed!!

--- askoverflow/src/components/Question/question.js
+++ askoverflow/src/components/Question/question.js
@@ -1,10 +1,153 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./homepage.css";
+import { NavForHome } from "../NavBar/NavBar";
+import { SideFeatured } from "../SideFeatured/sidefeatured"
+import { Questions } from "../Questions/questions"
+import { AskQuestion } from "../AskQuestion/askquestion"
+import $ from "jquery";
 
-class Question extends React.Component{
-  render(){
-    return (<div>
-      <h1>Question!!</h1>
-    </div>);
+class Question extends Component {
+  render() {
+    return (
+      <div>
+        <NavForHome />
+        {/* Grid System for questions */}
+        <div class="maincontent">
+          <div class="row">
+            {/*first grid*/}
+            <div class="col-sm-3 col-md-2 d-none d-sm-block">
+              <div class="just-padding">
+                <div class="list-group list-group-flush bg-light bg-gradient">
+                  <a
+                    href="#item1"
+                    class="list-group-item clickleftmenu bg-light bg-gradient"
+                    style={{ fontSize: "12px", textDecoration: "none" }}
+                    id="clickleftmenu1"
+                  >
+                    Home
+                  </a>
+
+                  <div
+                    class="list-group-item clickleftmenu bg-light bg-gradient"
+                    // data-toggle="collapse"
+                    style={{ fontSize: "12px", textDecoration: "none" }}
+                    id="clickleftmenu2"
+                  >
+                    PUBLIC
+                  </div>
+                  <div
+                    class="list-group list-group-flush bg-light bg-gradient"
+                    id="item-3"
+                  >
+                    <a
+                      href="#item31"
+                      id="clickleftmenu3"
+                      style={{ textDecoration: "none" }}
+                      class=" clickleftmenu list-group-item bg-light bg-gradient"
+                    >
+                      <i class="fas fa-globe-africa"></i>Questions
+                      <span class="badge countbadge badge-primary">9</span>
+                    </a>
+
+                    <a
+                      href="#item32"
+                      id="clickleftmenu4"
+                      style={{ textDecoration: "none" }}
+                      class="clickleftmenu list-group-item bg-light bg-gradient"
+                    >
+                      <i class="fas fa-tags"></i>Tags
+                      <span class="badge countbadge badge-primary">9</span>
+                    </a>
+
+                    <a
+                      href="#item33"
+                      id="clickleftmenu5"
+                      style={{ textDecoration: "none" }}
+                      class="clickleftmenu list-group-item bg-light bg-gradient"
+                    >
+                      <i class="fas fa-users"></i>Users
+                      <span class="badge countbadge badge-primary">9</span>
+                    </a>
+
+                    <a
+                      href="#item33"
+                      id="clickleftmenu6"
+                      style={{ textDecoration: "none" }}
+                      class="clickleftmenu list-group-item bg-light bg-gradient"
+                    >
+                      <i class="fas fa-briefcase"></i>Jobs
+                      <span class="badge countbadge badge-primary">9</span>
+                    </a>
+                  </div>
+                  <a
+                    href="#item1"
+                    class="list-group-item clickleftmenu bg-light bg-gradient"
+                    style={{ fontSize: "12px", textDecoration: "none" }}
+                    id="clickleftmenu7"
+                  >
+                    Teams
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/*second grid*/}
+            <div class="col-sm-9 col-md-10 col-12 bgmoredark cssforpadTomaincontent">
+              <div class="row">
+                <div class="col-lg-8">
+                  <div class="row margquesions">
+                    <div class="col-7 col-xl-9 col-lg-8 col-md-7">
+                      <h3 class="allquesionhead">Top Questions</h3>
+                    </div>
+                    <div class="col-5 col-xl-3 col-lg-4 col-md-5">
+                      <button class="btnaskquestion btn btn-secondary" data-bs-toggle="modal" data-bs-target="#askquestionmodal">Ask Question</button>
+                      <AskQuestion />
+                    </div>
+                  </div>
+
+                  <div class="row margquesions1">
+                    <div class="col-lg-4 col-xl-6 col-md-3">
+                      <h4 class="countallquesion">38 Questions</h4>
+                    </div>
+                    <div class="col-lg-5 col-xl-5 col-md-6">
+                      <div class="row">
+                        <ul class="list-group list-group-horizontal">
+                          <a href="#homepagelink" class="categoryonmaincontent list-group-item active list-group-item-action border border-secondary">Newest</a>
+                          <a href="#homepagelink" class="categoryonmaincontent list-group-item d-flex align-items-center list-group-item-action border border-secondary"><span class="badge badge-secondary">15</span>&nbsp;&nbsp;Top</a>
+                          <a href="#homepagelink" class="categoryonmaincontent list-group-item list-group-item-action border border-secondary">Views</a>
+                          <a href="#homepagelink" class="categoryonmaincontent list-group-item list-group-item-action border border-secondary">Oldest</a>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+
+
+                  <Questions />
+
+                  <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center">
+                      <li class="page-item disabled">
+                        <a class="page-link" href="#homepagelink" tabindex="-1">Previous</a>
+                      </li>
+                      <li class="page-item"><a class="page-link" href="#1">1</a></li>
+                      <li class="page-item"><a class="page-link" href="#1">2</a></li>
+                      <li class="page-item"><a class="page-link" href="#1">3</a></li>
+                      <li class="page-item">
+                        <a class="page-link" href="#homepagelink">Next</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div class="col-lg-4 bgmoredark">
+                  <br />
+                  <SideFeatured />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Question;
--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// const config = require('config');
+// const db = process.env.mongoURI;
+const db = process.env['mongoURI']
 
 const connectDB = async () => {
   try {
